6721149fea5150a77e2e2fe33b91a26bdf678a1e
simon <sidwyer@cisco.com>
2017-07-27 16:27:50 +1000

    Adding other variables from os env

--- CTPU/__init__.py
+++ CTPU/__init__.py
@@ -5,11 +5,11 @@ import requests
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py', silent=True)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#app.config['BOTTOKEN'] = os.environ['BOTTOKEN']
-#app.config['TUNNEL'] = os.environ['TUNNEL']
-#app.config['ADMIN'] = os.environ['ADMIN']
-#app.config['WEBHOOK_SECRET'] = os.environ['WEBHOOK_SECRET']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['BOTTOKEN'] = os.environ['BOTTOKEN']
+app.config['TUNNEL'] = os.environ['TUNNEL']
+app.config['ADMIN'] = os.environ['ADMIN']
+app.config['WEBHOOK_SECRET'] = os.environ['WEBHOOK_SECRET']
 db.init_app(app)
 
 def setHeaders():
