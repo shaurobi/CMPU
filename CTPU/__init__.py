a556a13ec1c989d6bdb3f623ae6bc0ac88ed7725
simon <sidwyer@cisco.com>
2017-07-27 11:49:55 +1000

    Silent file on config.py import

--- CTPU/__init__.py
+++ CTPU/__init__.py
@@ -4,7 +4,7 @@ import requests
 from CTPU.models import db, Person, Partner, Sendmessage
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', silent=True)
 db.init_app(app)
 
 
