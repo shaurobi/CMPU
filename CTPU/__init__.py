0a954e04f006083924f293796b25adf2060ea536
simon <sidwyer@cisco.com>
2017-08-01 07:42:53 +1000

    Adding other variables from os env

--- CTPU/__init__.py
+++ CTPU/__init__.py
@@ -38,7 +38,7 @@ def send_message_email(header, toPersonEmail, text):
     if toPersonEmail == "all":
         allusers = Person.query.all()
         for person in allusers:
-            message = {"toPersonEmail": person.email, "text": text}
+            message = {"toPersonEmail": person.email, "markdown": text}
             print("sending to" + person.email)
             requests.post(messageUrl, headers=header, json=message)
     else:
@@ -82,7 +82,7 @@ def send(webhook, message):
         elif dbstate.state == "emailadded":
             dbstate.message = message
             dbstate.state = "message added"
-            send_message_email(header, dbstate.to, dbstate.message)
+            send_message_email(header, dbstate.to, message)
             sendmessage(header, roomId, "Message has been sent 🤘")
             db.session.delete(dbstate)
             db.session.commit()
@@ -92,6 +92,7 @@ def send(webhook, message):
 
 def get_message(header, messageId):
     url = "https://api.ciscospark.com/v1/messages/"
+    print(messageId)
     r = requests.get(url + messageId, headers=header)
     r = r.json()
     return r['text']
